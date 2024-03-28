c5eb9b32f651fcedfdece0fe704f050bf204c853
PascalSeth <pascalelikem@gmail.com>
2024-03-28 10:32:46 +0000

    Error fixed

--- app/components/recent-cleaning.tsx
+++ app/components/recent-cleaning.tsx
@@ -85,20 +85,21 @@ export const columns: ColumnDef<Payment>[] = [
     id: "select",
     header: ({ table }) => (
       <Checkbox
-        checked={
-          table.getIsAllPageRowsSelected() ||
-          (table.getIsSomePageRowsSelected() && "indeterminate")
-        }
-        onCheckedChange={(value:string) => table.toggleAllPageRowsSelected(!!value)}
-        aria-label="Select all"
-      />
+      checked={
+        table.getIsAllPageRowsSelected() ||
+        (table.getIsSomePageRowsSelected() && "indeterminate")
+      }
+      onCheckedChange={(value: boolean) => table.toggleAllPageRowsSelected(value)}
+      aria-label="Select all"
+    />
     ),
     cell: ({ row }) => (
-      <Checkbox
-        checked={row.getIsSelected()}
-        onCheckedChange={(value:string) => row.toggleSelected(!!value)}
-        aria-label="Select row"
-      />
+<Checkbox
+  checked={row.getIsSelected()}
+  onCheckedChange={(value: boolean) => row.toggleSelected(value)}
+  aria-label="Select row"
+/>
+  
     ),
     enableSorting: false,
     enableHiding: false,
