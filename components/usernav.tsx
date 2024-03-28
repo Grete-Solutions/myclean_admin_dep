b6812bd536e22c684fd802637308b21d44df8231
PascalSeth <pascalelikem@gmail.com>
2024-03-28 21:20:50 +0000

    button removed

--- components/usernav.tsx
+++ components/usernav.tsx
@@ -8,11 +8,10 @@ export function UserNav() {
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
-                <Button variant="ghost" className="relative h-8 w-8 rounded-full">
                     <Avatar className="h-10 w-10">
                         <AvatarImage src="/avatars/02.png" alt="" />
                     </Avatar>
-                </Button>
+        
             </DropdownMenuTrigger>
             <DropdownMenuContent className="w-56 z-[99998]">
                 <DropdownMenuLabel>
