6e15becfe4f52e4869a4b72230ae2b404d67a6b4
PascalSeth <pascalelikem@gmail.com>
2024-03-30 08:04:33 +0000

    bg-white

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -15,7 +15,7 @@ export const SideBar = () => {
 
     const asideStyle = classNames({
         sidebar: true,
-        'overflow-y-auto overflow-x-auto fixed transition duration-300  ease-in-out z-[99999]': true,
+        'overflow-y-auto overflow-x-auto bg-white fixed transition duration-300  ease-in-out z-[99999]': true,
         'text-black': theme === 'light',
         'text-white': theme === 'dark',
         'text-sky-100': theme === 'custom',
