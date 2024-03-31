f082b83ab0bf4a6081855be5700a4a68a08ebe3c
PascalSeth <pascalelikem@gmail.com>
2024-03-31 16:43:27 +0000

    dashboaard

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import Earnings from './components/Earnings'
 export default function Home() {
   return (
     <>
-       <h2 className="text-3xl font-bold tracking-tight my-4">Dashboard( EFFUMWAA CINDY HELLO FROM EARTH NEXT TIME TAKE ANOTHER PLANET JORR)</h2>
+       <h2 className="text-3xl font-bold tracking-tight my-4">Dashboard</h2>
 
       <div className="flex-1 space-y-4 overflow-x-auto">
         <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
