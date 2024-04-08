dcbb33879c596857f0ef794d5bcd30a497b23a83
Bra Natty <stephenbedz@gmail.com>
2024-04-08 15:18:16 +0000

    Changed to Pickup

--- app/components/CancelledRides.tsx
+++ app/components/CancelledRides.tsx
@@ -39,7 +39,7 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
-import { Admin,data } from "../(pages)/PickupRequests/CancelledRides/data-table"
+import { Admin,data } from "../(pages)/TripRequests/CancelledRides/data-table"
 
 
 
