d947b0a2a713b124749835d574e1006f097c6ee4
PascalSeth <pascalelikem@gmail.com>
2024-05-09 02:06:26 +0000

    logo

--- app/(pages)/(login)/login/LoginLogo.tsx
+++ app/(pages)/(login)/login/LoginLogo.tsx
@@ -4,6 +4,6 @@ import Image from "next/image"
 export const LoginLogo=()=>
 {
     const { theme } = useTheme();
-return <Image width={35} alt="" className="mx-auto h-10 w-auto"
+return <Image width={35} alt="" className="mx-auto min-h-fit w-16 object-contain"
 height={35} src={theme === 'dark' || theme === 'custom' ? '/g1.png' : '/a1.png'}/>
 }
