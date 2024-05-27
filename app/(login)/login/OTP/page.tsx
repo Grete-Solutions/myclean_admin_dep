a9cf45a3103727c04e3dcfcc42caf6b4b2ea121b
PascalSeth <pascalelikem@gmail.com>
2024-05-27 19:50:04 +0000

    apox

--- app/(login)/login/OTP/page.tsx
+++ app/(login)/login/OTP/page.tsx
@@ -70,7 +70,7 @@ const Page: React.FC<Props> = () => {
                   </div>
 
                   <div className="flex flex-row items-center justify-center text-center text-sm font-medium space-x-1 text-gray-500">
-                    <p>Didn't receive code?</p>
+                    <p>Didn&apos;t receive code?</p>
                     <a className="flex flex-row items-center text-blue-600" href="http://" target="_blank" rel="noopener noreferrer">Resend</a>
                   </div>
                 </div>
