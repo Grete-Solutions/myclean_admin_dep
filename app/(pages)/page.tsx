1023b888ba9d3da0c8ca680ce00ddb88d538b576
PascalSeth <pascalelikem@gmail.com>
2024-06-16 09:18:31 +0000

    check

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,36 +1,20 @@
 'use client'
-import React, { useEffect } from 'react';
+import React from 'react';
 import Home from '../Dashboard';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 
 export default function Page() {
   const { data: session, status } = useSession();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (status === 'unauthenticated') {
-      router.replace('/login');
-    }
-  }, [status, router]);
-
-  useEffect(() => {
-    if (status === 'authenticated' && session.user.role !== 'authenticated') {
-      router.replace('/login');
-    }
-  }, [status, session, router]);
-
+  console.log(session)
+  const router = useRouter()
   if (status === 'loading') {
     return <div>Loading...</div>;
   }
 
-  if (status === 'authenticated' && session.user.role === 'authenticated') {
-    return (
-      <div>
-        <Home />
-      </div>
-    );
-  }
-
-  return null;
+  return (
+    <div>
+      <Home />
+    </div>
+  );
 }
--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,12 @@
 import { withAuth } from "next-auth/middleware"
 export default withAuth({
-  // Matches the pages config in `[...nextauth]`
   pages: {
     signIn: "/login",
     error: "/error",
   },
 })
 export const config = {  matcher: [
-    // "/",
+    "/",
     "/Configurations",
     "/Configurations/priveleges",
     "/Configurations/system-settings",
