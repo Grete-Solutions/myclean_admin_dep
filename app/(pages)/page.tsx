0d7d21a3f38718bea32f4465da65283d3bec1a04
PascalSeth <pascalelikem@gmail.com>
2024-06-30 03:14:52 +0000

    bew

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -11,7 +11,9 @@ export default function Page() {
   if (status === 'loading') {
     return <div>Loading...</div>;
   }
-
+  if (!session?.user){
+    router.replace('/login');
+    return null; }
   if ( session?.user) {
     return (
       <div>
